c5a3a2ac014ad20aa822a9938b6332c524c5e03d
isdigit <unriddler@outlook.com>
2022-10-25 00:43:54 -0500

    Added cell -Added cell class -Instantiated point objects within line class -Instantiated point objects and line objects within cell class -Refactored classes to be more consistent

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ class Window:
             height = self.height,
             width = self.width,
             cursor="cross")
-        self.canvas.pack()#fill = "both")
+        self.canvas.pack(fill = "both", expand=True)
         self.window_running = False
         self.root.protocol("WM_DELETE_WINDOW", self.close)
 
@@ -36,31 +36,87 @@ class Window:
 
 class Point:
     def __init__(self, x, y):
-        self.x = None #where 0 is the left of the screen
-        self.y = None #where 0 is the top of the screen
+        self.x = x #where 0 is the left of the screen
+        self.y = y #where 0 is the top of the screen
+
 
 class Line:
-    def __init__(self):
-        self.x1 = None
-        self.y1 = None
-        self.x2 = None
-        self.y3 = None
-        self.fill_color = None
-        self.width = None
+    def __init__(self, win, x1, y1, x2, y2, line_color = "black", width = 2):
+        self._win = win
+        self.pnt_1 = Point(x1, y1)
+        self.pnt_2 = Point(x2, y2)
+        self._line_color = line_color
+        self._width = width
 
-    def draw(self, canvas, x1, y1, x2, y2, fill_color = "black", width = 2):
-        canvas.create_line(x1, y1, x2, y2, fill=fill_color, width=2)
+    def draw(self):
+        self._win.create_line(self.pnt_1.x, self.pnt_1.y,
+                            self.pnt_2.x, self.pnt_2.y,
+                            fill=self._line_color, width=self._width)
+
+class Cell():
+    def __init__(self, win, row, col, x1, y1, x2, y2,
+                left_wall = True, right_wall = True,
+                top_wall = True, bottom_wall = True,
+                line_color="black", line_width = 2):
+        
+        self._win = win
+        self.row = row
+        self.col = col
+        self._pnt_1 = Point(x1, y1)
+        self._pnt_2 = Point(x2, y2)
+        self._line_color = line_color
+        self._line_width = line_width
+        self._left_wall = Line(self._win, self._pnt_1.x, self._pnt_1.y, self._pnt_1.x, self._pnt_2.y, self._line_color, self._line_width)
+        self._right_wall = Line(self._win, self._pnt_2.x, self._pnt_1.y, self._pnt_2.x, self._pnt_2.y, self._line_color, self._line_width)
+        self._top_wall = Line(self._win, self._pnt_1.x, self._pnt_1.y, self._pnt_2.x, self._pnt_1.y, self._line_color, self._line_width)
+        self._bottom_wall = Line(self._win, self._pnt_1.x, self._pnt_2.y, self._pnt_2.x, self._pnt_2.y, self._line_color, self._line_width)
+        self.has_left_wall = left_wall
+        self.has_right_wall = right_wall
+        self.has_top_wall = top_wall
+        self.has_bottom_wall = bottom_wall
+
+    def draw(self):
+        if self.has_left_wall:
+            self._left_wall.draw()
+        if self.has_right_wall:
+            self._right_wall.draw()
+        if self.has_top_wall:
+            self._top_wall.draw()
+        if self.has_bottom_wall:
+            self._bottom_wall.draw()
 
 def main():
     try:
-        win = Window(800,600)
-        Line().draw(win.canvas, 0, 0, 800, 600)
-        Line().draw(win.canvas, 0, 600, 800, 0)
-        Line().draw(win.canvas, 0,300,800,300)
-        Line().draw(win.canvas, 400,0,400,600)
+        grid_width=20
+        grid_height=20
+        cell_size = 25
+        win_width = 800
+        win_height = 600
+        min_border = cell_size
+        win = Window(win_width,win_height)
+        cells=[]
+
+    #build a list of cells
+        i = 0
+        while i < grid_width:
+            j = 0
+            while j < grid_height:
+                x1 = min_border+i*cell_size
+                y1 = min_border+j*cell_size
+                x2 = min_border+i*cell_size+cell_size
+                y2 = min_border+j*cell_size+cell_size
+                cells.append(Cell(win.canvas, j, i, x1, y1, x2, y2))
+                j += 1
+            i += 1
+
+     #draw all cells
+        for c in cells:
+            c.draw()
+
         win.wait_for_close()
+
     except Exception as exc:
-        print(f"!!SOMETHING WENT WRONG!!/N{exc}")
+        messagebox.showerror(message=f"!!SOMETHING WENT WRONG!!\n {exc}")
 
 main()
 
